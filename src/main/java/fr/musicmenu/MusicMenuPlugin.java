017fef684b79649c2343a00f46814a41eae3b07d
RuffenachNolan <116884893+RuffenachNolan@users.noreply.github.com>
2026-06-14 20:02:22 +0200

    Update MusicMenuPlugin.java

--- src/main/java/fr/musicmenu/MusicMenuPlugin.java
+++ src/main/java/fr/musicmenu/MusicMenuPlugin.java
@@ -1,5 +1,4 @@
 package fr.musicmenu;
-package fr.musicmenu;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,18 +7,13 @@ public class MusicMenuPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
-        // Commande principale
         getCommand("musicmenu").setExecutor(new MusicCommand());
-
-        // Events (clics menu)
         Bukkit.getPluginManager().registerEvents(new MenuListener(), this);
-
-        getLogger().info("MusicMenu activé ✔");
+        getLogger().info("MusicMenu activé !");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("MusicMenu désactivé ✔");
+        getLogger().info("MusicMenu désactivé !");
     }
 }
