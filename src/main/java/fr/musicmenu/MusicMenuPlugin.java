ad20e5b189eaa12f481e0455f04fc97088ba1f70
RuffenachNolan <116884893+RuffenachNolan@users.noreply.github.com>
2026-06-14 19:51:42 +0200

    Add files via upload

---
+++ src/main/java/fr/musicmenu/MusicMenuPlugin.java
@@ -0,0 +1,24 @@
+package fr.musicmenu;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MusicMenuPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        // Commande principale
+        getCommand("musicmenu").setExecutor(new MusicCommand());
+
+        // Events (clics menu)
+        Bukkit.getPluginManager().registerEvents(new MenuListener(), this);
+
+        getLogger().info("MusicMenu activé ✔");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("MusicMenu désactivé ✔");
+    }
+}
